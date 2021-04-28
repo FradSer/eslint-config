5313abdd82efdd13170957eb9568698c02b0fea2
Frad LEE <fradser@gmail.com>
2021-04-28 22:13:08 +0800

    feat: change rules
    - rules copy from https://github.com/FradSer/eslint-config-yuanqing
    - update eslint-plugin-simple-import-sort
    - fix sort-keys errors

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 module.exports = {
   extends: [
-    'standard',
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+    'plugin:import/warnings',
     'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: [
-    '@typescript-eslint',
-    // Runs prettier as an eslint rule
+    'plugin:react/recommended',
     'prettier'
   ],
   // See https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true
-    }
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+    'simple-import-sort',
+    'sort-keys-fix'
+  ],
+  rules: {
+    // Disable some rules from `@typescript-eslint`
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/member-delimiter-style': ['off'],
+    '@typescript-eslint/no-empty-function': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
+    // See https://github.com/lydell/eslint-plugin-simple-import-sort/blob/master/README.md#usage
+    'import/order': ['off'],
+    // See https://github.com/eslint/eslint/blob/master/docs/rules/no-console.md
+    'no-console': ['error'],
+    // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    'no-unused-vars': ['off'],
+    // See https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
+    'react/jsx-sort-props': [
+      'error',
+      {
+        ignoreCase: true,
+        reservedFirst: true
+      }
+    ],
+    // See https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    'react/no-unknown-property': ['off'],
+    // See https://github.com/facebook/react/blob/master/packages/eslint-plugin-react-hooks/README.md#installation
+    'react-hooks/exhaustive-deps': ['error'],
+    'react-hooks/rules-of-hooks': ['error'],
+    // See https://github.com/lydell/eslint-plugin-simple-import-sort#usage
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    // See https://github.com/leo-buneev/eslint-plugin-sort-keys-fix#usage
+    'sort-keys-fix/sort-keys-fix': [
+      'error',
+      'asc',
+      {
+        natural: true
+      }
+    ]
   },
   settings: {
     react: {
@@ -27,79 +69,5 @@ module.exports = {
       pragma: 'h',
       version: '16.0'
     }
-  },
-  rules: {
-    // var is block scoped, lets keep it that way
-    'block-scoped-var': 'error',
-    // Deeply nested ifs are a big smell
-    'max-depth': ['error', 2],
-    // Deeply nested code should be refactored & flattened out
-    'max-nested-callbacks': ['error', 4],
-    // Prevent hard to read code
-    'max-statements-per-line': ['error', { max: 2 }],
-    // Enforce a consistent comment styles
-    'multiline-comment-style': ['error', 'separate-lines'],
-    // Disallow use of alert dialog boxes
-    'no-alert': 'error',
-    // Disallow bitwise in general, if actually requried you can ignore this rule
-    'no-bitwise': 'error',
-    // Make sure variable name doesn't clash with argument name
-    'no-catch-shadow': 'error',
-    // Notify if console statements are left in by accident
-    'no-console': 'error',
-    // Helps disambiguate certain regexes
-    'no-div-regex': 'error',
-    // Use a single import statement
-    'no-duplicate-imports': 'error',
-    // Enforce more accurate equality check
-    'no-eq-null': 'error',
-    // Labels are not needed in this circumstance
-    'no-extra-label': 'error',
-    // Avoid polution global scope
-    'no-implicit-globals': 'error',
-    // Prefer else if
-    'no-lonely-if': 'error',
-    // Prevent creating functions in a loop
-    'no-loop-func': 'error',
-    // Make sure require calls are separate from other logic
-    'no-mixed-requires': 'error',
-    // Disallow chained assignment, difficult to read
-    'no-multi-assign': 'error',
-    // No need to use javascript:
-    'no-script-url': 'error',
-    // Prevent unused labels from being left behind
-    'no-unused-labels': 'error',
-    // Prevents unnecessary concat
-    'no-useless-concat': 'error',
-    // Prefer let or const (block scoped variables) over function scoped
-    'no-var': 'error',
-    // Warn about warning comments. Used to highlight TODO's etc in the codebase
-    'no-warning-comments': 'warn',
-    // Prefer shorthand objects
-    'object-shorthand': ['error', 'always'],
-    // Enforce whitespace between if/block statements (not dealt with by Prettier)
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: 'multiline-block-like', next: '*' },
-      { blankLine: 'always', prev: '*', next: 'multiline-block-like' }
-    ],
-    // Prefer { ... } over Object.assign
-    'prefer-object-spread': 'error',
-    // Prefer [...args] over .apply(obj, args)
-    'prefer-spread': 'error',
-    // Prefer template literals instead of string concatenation
-    'prefer-template': 'error',
-    // Prevent subtle race conditions
-    'require-atomic-updates': 'error',
-    // Highlight functions flagged as async but which don't use await
-    'require-await': 'error',
-    // Highlight functions flagged with an asterisk but which don't use yield
-    'require-yield': 'error',
-    // vars are hoisted so lets keep it clear
-    'vars-on-top': 'error',
-    // Enforce use of second parameter with parseInt/Float etc
-    radix: 'error',
-    // Enforce use of strict directive
-    strict: 'error'
   }
 };
